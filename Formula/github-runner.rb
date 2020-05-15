2e022920f001795355dfc28f30d3f5db2ccb2e7b
Luís Portela Afonso <luis.portela@ydata.ai>
2020-05-15 16:29:13 +0100

    fix: add script to setup env, path and services

--- Formula/github-runner.rb
+++ Formula/github-runner.rb
@@ -4,15 +4,36 @@ class GithubRunner < Formula
   url "https://github.com/actions/runner/releases/download/v2.169.1/actions-runner-osx-x64-2.169.1.tar.gz"
   head "https://github.com/actions/runner.git"
   sha256 "d24529d7016b6f281b8c0daa052215381075db377c73cc6221335e13bb9e4a07"
+  version "2.169.1"
 
   def install
 
     libexec.install Dir["bin"]
     libexec.install Dir["externals"]
 
+    ohai "Setting up environment"
+    system "./env.sh"
+
+    ohai "Moving .env and .path"
+    libexec.install ".env"
+    libexec.install ".path"
+
+    ohai "Write plist path to .service file"
+    (libexec/".service").write plist_path
+
     ln_s libexec/"bin/Runner.Listener", "github-runner"
 
     bin.install "github-runner"
+  end
+
+  def caveats
+    <<~EOS
+    To configure the runner, make sure you configure it with the url and token from github
+      github-runner configure --url <URL to repo|organization> --token <TOKEN>
+
+    To remove it from launchd
+      brew services stop github-runner
+    EOS
   end
 
   def plist
@@ -39,6 +60,8 @@ class GithubRunner < Formula
         <dict>
           <key>ACTIONS_RUNNER_SVC</key>
           <string>1</string>
+          <key>PATH</key>
+          <string>#{ENV["PATH"]}</string>
         </dict>
       </dict>
     </plist>
